1a253e9060ee5a618a87be18af541f6bb56a7af6
Adriano Raiano <adriano@raiano.ch>
2025-08-31 09:51:48 +0200

    docs update

--- docs/assets/react-18-B3ZL1uXK.js
+++ docs/assets/react-18-B3ZL1uXK.js
@@ -1,4 +1,4 @@
-import{a as kv,r as Fc}from"./iframe-kvA2sJUK.js";var wc={exports:{}},ie={},Wc={exports:{}},$c={};/**
+import{a as kv,r as Fc}from"./iframe-BbcMAxbW.js";var wc={exports:{}},ie={},Wc={exports:{}},$c={};/**
  * @license React
  * scheduler.production.js
  *
