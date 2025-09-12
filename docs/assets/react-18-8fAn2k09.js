77b1826f546035849123ab6c7219a0f422fdcd7c
Adriano Raiano <adriano@raiano.ch>
2025-09-12 14:11:39 +0200

    docs update

--- docs/assets/react-18-8fAn2k09.js
+++ docs/assets/react-18-8fAn2k09.js
@@ -1,4 +1,4 @@
-import{a as kv,r as Fc}from"./iframe-QT8oHMxz.js";var wc={exports:{}},ie={},Wc={exports:{}},$c={};/**
+import{a as kv,r as Fc}from"./iframe-Bc0s8Dvo.js";var wc={exports:{}},ie={},Wc={exports:{}},$c={};/**
  * @license React
  * scheduler.production.js
  *
